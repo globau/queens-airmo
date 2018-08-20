8c989b445312aec86d9651b3d8bd2ce940816cc9
Byron Jones <byron@glob.com.au>
2018-08-20 14:52:37 +0800

    fix dates in event frames

--- queens-airmo.js
+++ queens-airmo.js
@@ -1,20 +1,48 @@
+const date_selector = 'td[data=StartTimeDisplay], div.SlideItemTextLine2, .Date';
+
+function fix_dates(doc) {
+    let date_re = new RegExp(/^(\d+)\/(\d+)\/(\d\d\d\d.*)$/);
+    doc
+        .querySelectorAll(date_selector)
+        .forEach(function(el) {
+            var match = date_re.exec(el.textContent);
+            if (match) {
+                el.textContent = match[2] + '/' + match[1] + '/' + match[3];
+            }
+        });
+}
+
 let int_id = window.setInterval(function() {
+    // wait for main frame to appear in dom
     let floor_frame = document.querySelector('#ShowFloorFrame');
     if (!floor_frame) return;
     window.clearInterval(int_id);
 
-    let date_re = new RegExp(/^(\d+)\/(\d+)\/(\d\d\d\d.*)$/);
-    let on_floor_frame_load = function() {
-        floor_frame
-            .contentWindow
-            .document
-            .querySelectorAll('td[data=StartTimeDisplay], div.SlideItemTextLine2')
-            .forEach(function(el) {
-                var match = date_re.exec(el.textContent);
-                if (!match) return;
-                el.textContent = match[2] + '/' + match[1] + '/' + match[3];
-            });
-    };
-    floor_frame.addEventListener('load', on_floor_frame_load);
-    on_floor_frame_load();
-}, 250);
+    // fix dates when frame has loaded
+    floor_frame.addEventListener('load', () => {
+        fix_dates(floor_frame.contentWindow.document);
+
+        // watch for changes to dom (eg. event modal dialogs)
+        let observer = new MutationObserver((mutations) => {
+            for (let mutation of mutations) {
+                // listen for eventdiv added to dom
+                if (mutation.type !== 'childList' || !mutation.addedNodes) continue;
+                for (let added_node of mutation.addedNodes) {
+                    if (added_node.id !== 'EventDiv') continue;
+
+                    // when the event-frame loads, fix its dates too
+                    let event_frame = added_node.querySelector('#EventFrame');
+                    if (event_frame) {
+                        event_frame.addEventListener('load', () => {
+                            fix_dates(event_frame.contentWindow.document);
+                        });
+                    }
+                }
+            }
+        });
+        observer.observe(
+            floor_frame.contentWindow.document.body,
+            { attributes: false, childList: true, subtree: false },
+        );
+    });
+}, 50);
